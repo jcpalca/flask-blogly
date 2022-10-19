849c1744efac0e82dbcc2020df94624c03993c3f
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-19 12:29:15 -0700

    Add debug toolbar and reset table

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 """Blogly application."""
 
 from flask import Flask
-from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
+db.drop_all()
 db.create_all()
