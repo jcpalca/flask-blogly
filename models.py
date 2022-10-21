61771f41aeb9468e6e0ac749051a6da8a9479345
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-20 17:26:00 -0700

    Create Post class for database table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 """Models for Blogly."""
-
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -30,3 +30,25 @@ class User(db.Model):
     image_url = db.Column(db.String(100),
                           nullable = False,
                           default = DEFAULT_IMG_URL)
+
+    posts = db.relationship("Post", backref="user")
+
+class Post(db.Model):
+    """Post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key = True,
+                   autoincrement = True)
+    title = db.Column(db.String(25),
+                      nullable = False,
+                      unique = True)
+    content = db.Column(db.String(1600),
+                        nullable = False)
+    created_at = db.Column(db.DateTime,
+                        nullable = False,
+                        default = datetime.datetime.now),
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable = False)
